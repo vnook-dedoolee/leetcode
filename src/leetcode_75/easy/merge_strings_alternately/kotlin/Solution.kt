fc177859ffd157329a181c4eb9112fc183635a3c
vnook_dedoolee <now-kirill@ya.ru>
2026-09-22 13:03:33 +0300

    Merge Strings Alternately

---
+++ src/leetcode_75/easy/merge_strings_alternately/kotlin/Solution.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val totalLength = word1.length + word2.length
+        val result = StringBuilder()
+        for (i in 0 until totalLength) {
+            if (i < word1.length) {
+                result.append(word1[i])
+            }
+            if (i < word2.length) {
+                result.append(word2[i])
+            }
+        }
+        return result.toString()
+    }
+}
